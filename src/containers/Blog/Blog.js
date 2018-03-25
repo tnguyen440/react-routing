1593968e88f1750bc5057aa737ee2e27495695b9
Minh Tien Nguyen <tien.minh@hubcba.com>
2018-03-25 21:33:32 +0700

    init

---
+++ src/containers/Blog/Blog.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import { Route, Link } from "react-router-dom";
+// import axios from 'axios';
+// import axios from "../../axios";
+
+// import Post from '../../components/Post/Post';
+// import FullPost from '../../components/FullPost/FullPost';
+// import NewPost from '../../components/NewPost/NewPost';
+import Posts from "./Posts/Posts";
+import NewPost from "./NewPost/NewPost";
+import "./Blog.css";
+
+class Blog extends Component {
+  render() {
+    return (
+      <div className="Blog">
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to={{
+                  pathname: '/new-post',
+                  hash: '#submit',
+                  seartch: '?quick-submit=true'
+                }}>New Post</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        {/* <Route path="/" exact render={() => <h1>Home</h1>} />
+        <Route path="/" render={() => <h1>Home2</h1>} /> */}
+        <Route path='/' exact component={Posts} />
+        <Route path='/new-post' component={NewPost} />
+      </div>
+    );
+  }
+}
+
+export default Blog;
